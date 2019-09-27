9507cbe6e8447c965792b12df7929ed73d33997d
Ben <benclarkphoto@mac.com>
2019-09-26 23:06:42 -0700

    add remove method

--- tree/index.js
+++ tree/index.js
@@ -7,4 +7,10 @@ class Node {
   add(data) {
     this.children.push(new Node(data));
   }
+
+  remove(data) {
+    this.children = this.children.filter(node => {
+      return node.data !== data;
+    })
+  }
 }
