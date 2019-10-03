1803bd3aa1f8ee89b26c16cd7d4b3cf10203c7a0
Ben <benclarkphoto@mac.com>
2019-10-02 17:18:31 -0700

    event handler on

---
+++ events/index.js
@@ -0,0 +1,17 @@
+
+
+class Events {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, callback) {
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
+  }
+
+  
+}
