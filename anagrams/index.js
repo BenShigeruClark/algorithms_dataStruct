3d5b052cb7b9ddc6eaacd5f7eabcc2c9e8611377
Ben <benclarkphoto@mac.com>
2019-11-20 22:20:23 -0800

    Added comments

--- anagrams/index.js
+++ anagrams/index.js
@@ -13,9 +13,44 @@ function anagrams(stringA, stringB) {
 }
 
 function cleanString(str) {
-  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
 }
+
+console.log(anagrams('hiya', 'iyha'))
+console.log(anagrams('what', 'musk'))
 
 // module.exports = anagrams;
 
-console.log(anagrams('he', 'eh'))
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+// //
+// function buildCharMap(str) {
+//   const charMap = {};
+
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//   return charMap;
+// }
+
+// console.log(anagrams('hell', 'lehl'))
